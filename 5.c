9dc4e9b6e527c05303072669f6e2f62c77d87b61
ASIF <asifistiak@ASIFs-MacBook-Air.local>
2026-01-21 19:45:05 +0600

    Asif

---
+++ 5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8;
+    scanf("%c %c %c %c %c %c %c %c",&ch1,&ch2,&ch3,&ch4,&ch5,&ch6,&ch7,&ch8);
+
+    printf("%c %c %c %c %c %c %c %c",ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8);
+    
+
+    return 0;
+}
---
+++ c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8,ch9,ch10;
+    scanf("%c %c %c %c %c %c %c %c %c %c ",&ch1,&ch2,&ch3,&ch4,&ch5,&ch6,&ch7,&ch8,&ch9,&ch10);
+    printf("%c%c%c%c%c%c%c%c%c%c",ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8,ch9,ch10);
+
+
+    return 0;
+}
